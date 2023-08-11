df186c9d744adb85b72b087a9b38125cf10a4fd1
Billy Ti <78161308+Billy-Ti@users.noreply.github.com>
2023-08-11 17:29:22 +0800

    Update: add READ.ME file / modify config file

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // base 為編譯路徑 : 判斷是開發中 (npm run dev) 還是產品路徑 ( npm run build )
+  // process 為環境變數取得的方法
+  // .env.NODE_ENV 為當前 node 所編譯的環境
+  base: process.env.NODE_ENV === 'production' ? '/React-Vite-Project/' : '/',
   plugins: [react()],
 })
